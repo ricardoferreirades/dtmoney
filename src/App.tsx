0fc9c96537b96040ab88195d4a502bc28a1d1796
Ricardo Ferreira <ricardoferreirades@gmail.com>
2022-03-11 17:08:23 +0100

    chore: udate app component
    Signed-off-by: Ricardo Ferreira <ricardoferreirades@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <h1>Hi Ricardo</h1>
+
+      <button type="button" disabled={true}>
+        button
+      </button>
+
+      <GlobalStyle />
     </div>
   );
 }
