3c3ee92df589621846f831841e188b03d5566391
Ricardo Ferreira <ricardoferreirades@gmail.com>
2022-03-28 18:50:07 +0200

    feat: add new transaction modal
    Signed-off-by: Ricardo Ferreira <ricardoferreirades@gmail.com>

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
+import Modal from "react-modal";
 import logoImg from "../../assets/logo.svg";
 import { Container, Content } from "./styles";
-export function Header() {
+
+interface HeaderProps {
+  onNewTransactionModalOpen: () => void;
+}
+
+export function Header({ onNewTransactionModalOpen }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dtmoney logo" />
-        <button type="button">New Transaction</button>
+        <button type="button" onClick={onNewTransactionModalOpen}>
+          New Transaction
+        </button>
       </Content>
     </Container>
   );
