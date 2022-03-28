e94cda0f12d1d684f4c074dd1183b9346df2ccfc
Ricardo Ferreira <ricardoferreirades@gmail.com>
2022-03-28 21:35:19 +0200

    chore: add mock data base data by default
    Signed-off-by: Ricardo Ferreira <ricardoferreirades@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createServer } from "miragejs";
+import { createServer, Model } from "miragejs";
 import { App } from "./App";
 
 createServer({
+  models: {
+    transactions: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "One",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2021-02-12 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Two",
+          type: "withdraw",
+          category: "Casa",
+          amount: 6000,
+          createdAt: new Date("2021-02-12 11:00:00"),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = "api";
 
     this.get("/transactions", () => {
-      return [
-        {
-          id: 1,
-          title: "Transaction title",
-          amount: 400,
-          type: "deposit",
-          category: "food",
-          createdAt: new Date(),
-        },
-      ];
+      return this.schema.all("transactions");
+    });
+
+    this.post("transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create("transaction", data);
     });
   },
 });
